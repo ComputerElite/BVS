af9491da96a96c21b113e18b82f0b151499000e3
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2020-10-31 15:00:16 +0100

    Add files via upload

---
+++ C#/MainWindow.xaml.cs
@@ -0,0 +1,398 @@
+﻿using SimpleJSON;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Beat_Saber_Version_Switcher
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int MajorV = 1;
+        int MinorV = 0;
+        int PatchV = 0;
+        Boolean Preview = false;
+
+        Boolean draggable = true;
+        String exe = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.Length - 1);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (!Directory.Exists(exe + "\\Backups")) Directory.CreateDirectory(exe + "\\Backups");
+            if (!Directory.Exists(exe + "\\tmp")) Directory.CreateDirectory(exe + "\\tmp");
+            if (File.Exists(exe + "\\BVS_Update.exe")) File.Delete(exe + "\\BVS_Update.exe");
+            UpdateB.Visibility = Visibility.Hidden;
+            //StartBMBF();
+            txtbox.Text = "Output:";
+            Update();
+        }
+
+
+        private void Drag(object sender, RoutedEventArgs e)
+        {
+            bool mouseIsDown = System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed;
+
+
+            if (mouseIsDown)
+            {
+                if (draggable)
+                {
+                    this.DragMove();
+                }
+
+            }
+
+        }
+
+        public void noDrag(object sender, MouseEventArgs e)
+        {
+            draggable = false;
+        }
+
+        public void doDrag(object sender, MouseEventArgs e)
+        {
+            draggable = true;
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(exe + "\\tmp"))
+                {
+                    Directory.Delete(exe + "\\tmp", true);
+                }
+            }
+            catch
+            {
+            }
+            this.Close();
+        }
+
+        public void StartBMBF()
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate
+            {
+                adb("shell am start -n com.weloveoculus.BMBF/com.weloveoculus.BMBF.MainActivity");
+            }));
+        }
+
+        public Boolean adb(String Argument)
+        {
+            String User = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            ProcessStartInfo s = new ProcessStartInfo();
+            s.CreateNoWindow = false;
+            s.UseShellExecute = false;
+            s.FileName = "adb.exe";
+            s.WindowStyle = ProcessWindowStyle.Minimized;
+            s.Arguments = Argument;
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(s))
+                {
+                    exeProcess.WaitForExit();
+                    return true;
+                }
+            }
+            catch
+            {
+
+                ProcessStartInfo se = new ProcessStartInfo();
+                se.CreateNoWindow = false;
+                se.UseShellExecute = false;
+                se.FileName = User + "\\AppData\\Roaming\\SideQuest\\platform-tools\\adb.exe";
+                se.WindowStyle = ProcessWindowStyle.Minimized;
+                se.Arguments = Argument;
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(se))
+                    {
+                        exeProcess.WaitForExit();
+                        return true;
+                    }
+                }
+                catch
+                {
+                    // Log error.
+                    txtbox.AppendText("\n\n\nAn error Occured (Code: ADB100). Check following");
+                    txtbox.AppendText("\n\n- Your Quest is connected and USB Debugging enabled.");
+                    txtbox.AppendText("\n\n- You have adb installed.");
+                }
+            }
+            return false;
+        }
+
+        public String adbS(String Argument)
+        {
+            String User = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            ProcessStartInfo s = new ProcessStartInfo();
+            s.CreateNoWindow = false;
+            s.UseShellExecute = false;
+            s.FileName = "adb.exe";
+            s.WindowStyle = ProcessWindowStyle.Minimized;
+            s.RedirectStandardOutput = true;
+            s.Arguments = Argument;
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(s))
+                {
+                    String IPS = exeProcess.StandardOutput.ReadToEnd();
+                    exeProcess.WaitForExit();
+                    return IPS;
+                }
+            }
+            catch
+            {
+
+                ProcessStartInfo se = new ProcessStartInfo();
+                se.CreateNoWindow = false;
+                se.UseShellExecute = false;
+                se.FileName = User + "\\AppData\\Roaming\\SideQuest\\platform-tools\\adb.exe";
+                se.WindowStyle = ProcessWindowStyle.Minimized;
+                se.RedirectStandardOutput = true;
+                se.Arguments = Argument;
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(se))
+                    {
+                        String IPS = exeProcess.StandardOutput.ReadToEnd();
+                        exeProcess.WaitForExit();
+                        return IPS;
+
+                    }
+                }
+                catch
+                {
+                    // Log error.
+                    txtbox.AppendText("\n\n\nAn error Occured (Code: ADB100). Check following");
+                    txtbox.AppendText("\n\n- Your Quest is connected and USB Debugging enabled.");
+                    txtbox.AppendText("\n\n- You have adb installed.");
+                }
+            }
+            return "";
+        }
+
+        public void Update()
+        {
+            try
+            {
+                //Download Update.txt
+                using (WebClient client = new WebClient())
+                {
+                    try
+                    {
+                        client.DownloadFile("https://raw.githubusercontent.com/ComputerElite/BVS/main/Update.txt", exe + "\\tmp\\Update.txt");
+                    }
+                    catch
+                    {
+                        txtbox.AppendText("\n\n\nAn error Occured (Code: UD100). Couldn't check for Updates. Check following");
+                        txtbox.AppendText("\n\n- Your PC has internet.");
+                    }
+                }
+                StreamReader VReader = new StreamReader(exe + "\\tmp\\Update.txt");
+
+                String line;
+                int l = 0;
+
+                int MajorU = 0;
+                int MinorU = 0;
+                int PatchU = 0;
+                while ((line = VReader.ReadLine()) != null)
+                {
+                    if (l == 0)
+                    {
+                        String URL = line;
+                    }
+                    if (l == 1)
+                    {
+                        MajorU = Convert.ToInt32(line);
+                    }
+                    if (l == 2)
+                    {
+                        MinorU = Convert.ToInt32(line);
+                    }
+                    if (l == 3)
+                    {
+                        PatchU = Convert.ToInt32(line);
+                    }
+                    l++;
+                }
+
+                if (MajorU > MajorV || MinorU > MinorV || PatchU > PatchV)
+                {
+                    //Newer Version available
+                    UpdateB.Visibility = Visibility.Visible;
+                }
+
+                String MajorVS = Convert.ToString(MajorV);
+                String MinorVS = Convert.ToString(MinorV);
+                String PatchVS = Convert.ToString(PatchV);
+                String MajorUS = Convert.ToString(MajorU);
+                String MinorUS = Convert.ToString(MinorU);
+                String PatchUS = Convert.ToString(PatchU);
+
+                String VersionVS = MajorVS + MinorVS + PatchVS;
+                int VersionV = Convert.ToInt32(VersionVS);
+                String VersionUS = MajorUS + MinorUS + PatchUS + " ";
+                int VersionU = Convert.ToInt32(VersionUS);
+                if (VersionV > VersionU)
+                {
+                    //Newer Version that hasn't been released yet
+                    txtbox.AppendText("\n\nLooks like you have a preview version. Downgrade now from " + MajorV + "." + MinorV + "." + PatchV + " to " + MajorU + "." + MinorU + "." + PatchU + " xD");
+                    UpdateB.Visibility = Visibility.Visible;
+                    UpdateB.Content = "Downgrade Now xD";
+                }
+                if (VersionV == VersionU && Preview)
+                {
+                    //User has Preview Version but a release Version has been released
+                    txtbox.AppendText("\n\nLooks like you have a preview version. The release version has been released. Please Update now. ");
+                    UpdateB.Visibility = Visibility.Visible;
+                }
+                VReader.Close();
+            }
+            catch
+            {
+
+            }
+            try
+            {
+                File.Delete(exe + "\\tmp\\Update.txt");
+            }
+            catch
+            {
+            }
+        }
+
+        private void Start_Update(object sender, RoutedEventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile("https://github.com/ComputerElite/BVS/raw/main/BVS_Update.exe", exe + "\\BVS_Update.exe");
+            }
+            //Process.Start(exe + "\\QSU_Update.exe");
+            ProcessStartInfo s = new ProcessStartInfo();
+            s.CreateNoWindow = false;
+            s.UseShellExecute = false;
+            s.FileName = exe + "\\BVS_Update.exe";
+            try
+            {
+                using (Process exeProcess = Process.Start(s))
+                {
+                }
+                this.Close();
+            }
+            catch
+            {
+                // Log error.
+                txtbox.AppendText("\n\n\nAn error Occured (Code: UD200). Couldn't download Update.");
+            }
+        }
+
+        private void Mini(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void modded(object sender, RoutedEventArgs e)
+        {
+            //Install the modded Version of Beat Saber
+            if(!File.Exists(exe + "\\Backups\\modded.apk"))
+            {
+                txtbox.AppendText("\n\nYou have to click the unmod button first.");
+                return;
+            }
+            adb("pull /sdcard/Android/data/com.beatgames.beatsaber/files/LocalDailyLeaderboards.dat \"" + exe + "\\Backups\\files\\LocalDailyLeaderboards.dat\"");
+            adb("pull /sdcard/Android/data/com.beatgames.beatsaber/files/LocalLeaderboards.dat \"" + exe + "\\Backups\\files\\LocalLeaderboards.dat\"");
+            adb("pull /sdcard/Android/data/com.beatgames.beatsaber/files/PlayerData.dat \"" + exe + "\\Backups\\files\\PlayerData.dat\"");
+            adb("pull /sdcard/Android/data/com.beatgames.beatsaber/files/AvatarData.dat \"" + exe + "\\Backups\\files\\AvatarData.dat\"");
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nUninstalling Beat Saber.");
+                txtbox.ScrollToEnd();
+            }));
+            if (!adb("uninstall com.beatgames.beatsaber")) return;
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nInstalling Modded Beat Saber");
+                txtbox.ScrollToEnd();
+            }));
+            if (!adb("install \"" + exe + "\\Backups\\modded.apk\"")) return;
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nRestoring Game Data");
+                txtbox.ScrollToEnd();
+            }));
+            if (!adb("push \"" + exe + "\\Backups\\files\" /sdcard/Android/data/com.beatgames.beatsaber/files")) return;
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nGranting Permissions");
+                txtbox.ScrollToEnd();
+            }));
+            adb("shell pm grant com.beatgames.beatsaber android.permission.READ_EXTERNAL_STORAGE"); //Grant permission read
+            adb("shell pm grant com.beatgames.beatsaber android.permission.WRITE_EXTERNAL_STORAGE"); //Grant permission write
+            Directory.Delete(exe + "\\Backups", true);
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nfinished. You can now play your Custom Songs again.");
+                txtbox.ScrollToEnd();
+            }));
+        }
+
+        private void unmodded(object sender, RoutedEventArgs e)
+        {
+            //Install the unmodded Version of Beat Saber
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nBacking up everything.");
+                txtbox.ScrollToEnd();
+            }));
+            if (!adb("pull /sdcard/BMBFData/Backups/beatsaber-unmodded.apk \"" + exe + "\\tmp\\unmodded.apk\"")) return;
+            if (!adb("pull /sdcard/Android/data/com.beatgames.beatsaber/files \"" + exe + "\\Backups\"")) return;
+            //Directory.Delete(exe + "\\Backups\\files\\mods", true);
+            //Directory.Delete(exe + "\\Backups\\files\\libs", true);
+            String moddedBS = adbS("shell pm path com.beatgames.beatsaber").Replace("package:", "").Replace(System.Environment.NewLine, "");
+            txtbox.AppendText("\n\n\n" + moddedBS + "\n\npull " + moddedBS + "\"" + exe + "\\Backups\"");
+            if (!adb("pull " + moddedBS + " \"" + exe + "\\Backups\\modded.apk\"")) return;
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nInstalling unmodded Beat Saber.");
+                txtbox.ScrollToEnd();
+            }));
+            if (!adb("uninstall com.beatgames.beatsaber")) return;
+            if (!adb("install \"" + exe + "\\tmp\\unmodded.apk\"")) return;
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nRestoring Scores");
+                txtbox.ScrollToEnd();
+            }));
+            adb("push \"" + exe + "\\Backups\\files\\LocalDailyLeaderboards.dat\" /sdcard/Android/data/com.beatgames.beatsaber/files/LocalDailyLeaderboards.dat");
+            adb("push \"" + exe + "\\Backups\\files\\LocalLeaderboards.dat\" /sdcard/Android/data/com.beatgames.beatsaber/files/LocalLeaderboards.dat");
+            adb("push \"" + exe + "\\Backups\\files\\PlayerData.dat\" /sdcard/Android/data/com.beatgames.beatsaber/files/PlayerData.dat");
+            adb("push \"" + exe + "\\Backups\\files\\AvatarData.dat\" /sdcard/Android/data/com.beatgames.beatsaber/files/AvatarData.dat");
+
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate {
+                txtbox.AppendText("\n\nFinished. You can now play vanilla Beat Saber.");
+                txtbox.ScrollToEnd();
+            }));
+        }
+    }
+}
